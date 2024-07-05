2d43a70e781f6561bce063fd39b1143830c6b0d1
emilyare5 <emilyarellano1819@gmail>
2024-07-05 15:35:34 -0400

    feat: routes set up

---
+++ api/index.js
@@ -0,0 +1,11 @@
+// get the express package setup
+const express = require("express")
+
+// create the router
+const apiRouter = express.Router()
+
+// the route user and party
+apiRouter.get("/")
+
+// doing a module exports
+module.exports = apiRouter
